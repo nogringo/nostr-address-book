b0bf5e0ae18366d7ef5cd049de9b5e14a6fa1ef9
Nogringo <npub1kg4sdvz3l4fr99n2jdz2vdxe2mpacva87hkdetv76ywacsfq5leqquw5te@uid.ovh>
2026-06-08 10:58:40 +0200

    first commit

---
+++ example/nostr_address_book_example.dart
@@ -0,0 +1,6 @@
+import 'package:nostr_address_book/nostr_address_book.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/nostr_address_book_test.dart
@@ -0,0 +1,16 @@
+import 'package:nostr_address_book/nostr_address_book.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
